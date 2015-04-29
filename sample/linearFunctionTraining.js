7c695d060e6300d530ccc8f6663464242f2265de
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-04-29 19:52:31 +0200

    better tests

--- sample/linearFunctionTraining.js
+++ sample/linearFunctionTraining.js
@@ -5,7 +5,7 @@
 var nk = require( '../lib/neuronsKit.js' ) ;
 var term = require( 'terminal-kit' ).terminal ;
 
-var i , x , y , output , a , b ;
+var i , x , y , output , a , b , c ;
 
 
 
@@ -18,11 +18,12 @@ network.addInput( 'x' , inputX ) ;
 network.addInput( 'y' , inputY ) ;
 network.addOutput( 'output' , neuron ) ;
 
-a = Math.floor( 1 + Math.random() * 10 ) ;
-b = Math.floor( 1 + Math.random() * 10 ) ;
+a = Math.floor( -10 + Math.random() * 21 ) ;
+b = Math.floor( -10 + Math.random() * 21 ) ;
+//c = Math.floor( 1 + Math.random() * 10 ) ;
 
-inputX.connectTo( neuron , Math.random() * 10 ) ;
-inputY.connectTo( neuron , Math.random() * 10 ) ;
+inputX.connectTo( neuron , -10 + Math.random() * 20 ) ;
+inputY.connectTo( neuron , -10 + Math.random() * 20 ) ;
 
 console.log( "\nFunction wanted: f(x,y) = %sx+%sy\n" , a , b ) ;
 
@@ -31,8 +32,8 @@ for ( i = 0 ; i < 100 ; i ++ )
 	console.log( "\n-------- #%s -------- f(x,y) = %sx+%sy --------" , i , a , b ) ;
 	console.log( "Wx: %s , Wy: %s" , network.outputs.output.synapses[ 0 ].weight , network.outputs.output.synapses[ 1 ].weight  ) ;
 	
-	x = Math.floor( 1 + Math.random() * 10 ) ;
-	y = Math.floor( 1 + Math.random() * 10 ) ;
+	x = -10 + Math.random() * 20 ;
+	y = -10 + Math.random() * 20 ;
 	
 	network.setInputSignals( { x: x , y: y } ) ;
 	output = network.getOutputSignal( 'output' ) ;
