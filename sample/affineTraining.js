9e988afd51af36ebb81ef4137b95bb106401b7c2
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-05-01 12:48:59 +0200

    bias/threshold update

--- sample/affineTraining.js
+++ sample/affineTraining.js
@@ -12,7 +12,7 @@ var i , x , y , output , a , b , c ;
 var network = nk.createNetwork() ;
 	inputX = nk.createSignalEmitter() ,
 	inputY = nk.createSignalEmitter() ,
-	neuron = nk.createNeuron( { transfer: 'linear' , threshold: 0 } ) ;
+	neuron = nk.createNeuron( { transfer: 'linear' , threshold: Math.floor( -10 + Math.random() * 21 ) } ) ;
 
 network.addInput( 'x' , inputX ) ;
 network.addInput( 'y' , inputY ) ;
@@ -20,7 +20,7 @@ network.addOutput( 'output' , neuron ) ;
 
 a = Math.floor( -10 + Math.random() * 21 ) ;
 b = Math.floor( -10 + Math.random() * 21 ) ;
-//c = Math.floor( 1 + Math.random() * 10 ) ;
+constant = Math.floor( 1 + Math.random() * 10 ) ;
 
 inputX.connectTo( neuron , -10 + Math.random() * 20 ) ;
 inputY.connectTo( neuron , -10 + Math.random() * 20 ) ;
@@ -29,8 +29,11 @@ console.log( "\nFunction wanted: f(x,y) = %sx+%sy\n" , a , b ) ;
 
 for ( i = 0 ; i < 100 ; i ++ )
 {
-	console.log( "\n-------- #%s -------- f(x,y) = %sx+%sy --------" , i , a , b ) ;
-	console.log( "Wx: %s , Wy: %s" , network.outputs.output.synapses[ 0 ].weight , network.outputs.output.synapses[ 1 ].weight  ) ;
+	console.log( "\n-------- #%s -------- f(x,y) = %sx + %sy + %s --------" , i , a , b , constant ) ;
+	console.log( "Wx: %s , Wy: %s , bias: %s" ,
+		network.outputs.output.synapses[ 0 ].weight ,
+		network.outputs.output.synapses[ 1 ].weight ,
+		- network.outputs.output.threshold ) ;
 	
 	x = -10 + Math.random() * 20 ;
 	y = -10 + Math.random() * 20 ;
@@ -38,7 +41,7 @@ for ( i = 0 ; i < 100 ; i ++ )
 	network.setInputSignals( { x: x , y: y } ) ;
 	output = network.getOutputSignal( 'output' ) ;
 	
-	expected = a * x + b * y ;
+	expected = a * x + b * y + constant ;
 	console.log( "x: %s , y: %s , expected: %s , output: %s" , x , y , expected , output ) ;
 	
 	network.outputs.output.backSignals = [ { signal: expected , weight: 1 } ] ;
