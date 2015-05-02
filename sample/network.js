8cbcefdf4255e19d61cd39ca3cdbcec01de4b7a1
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-05-02 12:55:53 +0200

    wip

--- sample/network.js
+++ sample/network.js
@@ -10,15 +10,36 @@ var term = require( 'terminal-kit' ).terminal ;
 var network = nk.createNetwork() ;
 	inputA = nk.createSignalEmitter() ,
 	inputB = nk.createSignalEmitter() ,
-	neuron = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ;
+	outputA = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
+	outputB = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
+	hidden1 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
+	hidden2 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ;
 
 network.addInput( 'a' , inputA ) ;
 network.addInput( 'b' , inputB ) ;
-network.addOutput( 'output' , neuron ) ;
+network.addOutput( 'outputA' , outputA ) ;
+network.addOutput( 'outputB' , outputB ) ;
+network.addHiddenNeuron( hidden1 ) ;
+network.addHiddenNeuron( hidden2 ) ;
 
-inputA.connectTo( neuron , 1 ) ;
-inputB.connectTo( neuron , 1 ) ;
+inputA.connectTo( hidden1 , 1 ) ;
+inputA.connectTo( hidden2 , 1 ) ;
+inputB.connectTo( hidden1 , 1 ) ;
+inputB.connectTo( hidden2 , 1 ) ;
 
+hidden1.connectTo( outputA , 1 ) ;
+hidden1.connectTo( outputB , 1 ) ;
+hidden2.connectTo( outputA , 1 ) ;
+hidden2.connectTo( outputB , 1 ) ;
+
+network.feedForwardOrder() ;
+
+console.log( network.neuronOrder.indexOf( hidden1 ) ) ;
+console.log( network.neuronOrder.indexOf( hidden2 ) ) ;
+console.log( network.neuronOrder.indexOf( outputA ) ) ;
+console.log( network.neuronOrder.indexOf( outputB ) ) ;
+
+/*
 network.setInputSignals( {
 	a: 0 ,
 	b: 0
@@ -27,6 +48,6 @@ network.setInputSignals( {
 //console.log( '>>>>>' , network.inputs ) ;
 
 console.log( '\nOutput: ' , network.getOutputSignal( 'output' ) ) ;
-
+*/
 
 
