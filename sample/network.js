735c6c97f9467a6ac330f4a54801c60f1c4822a4
Cédric Ronvel <cedric.ronvel@cloud-solutions.fr>
2015-05-02 14:55:34 +0200

    .feedForwardOrder(): OK!

--- sample/network.js
+++ sample/network.js
@@ -11,31 +11,44 @@ var network = nk.createNetwork() ;
 	inputA = nk.createSignalEmitter() ,
 	inputB = nk.createSignalEmitter() ,
 	outputA = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
-	outputB = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
-	hidden1 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
-	hidden2 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ;
+	outputB = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ;
+
+var	hidden21 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
+	hidden22 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ;
+
+var	hidden11 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
+	hidden12 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ;
 
 network.addInput( 'a' , inputA ) ;
 network.addInput( 'b' , inputB ) ;
 network.addOutput( 'outputA' , outputA ) ;
 network.addOutput( 'outputB' , outputB ) ;
-network.addHiddenNeuron( hidden1 ) ;
-network.addHiddenNeuron( hidden2 ) ;
+network.addHiddenNeuron( hidden21 ) ;
+network.addHiddenNeuron( hidden22 ) ;
+network.addHiddenNeuron( hidden11 ) ;
+network.addHiddenNeuron( hidden12 ) ;
 
-inputA.connectTo( hidden1 , 1 ) ;
-inputA.connectTo( hidden2 , 1 ) ;
-inputB.connectTo( hidden1 , 1 ) ;
-inputB.connectTo( hidden2 , 1 ) ;
+inputA.connectTo( hidden11 , 1 ) ;
+inputA.connectTo( hidden12 , 1 ) ;
+inputB.connectTo( hidden11 , 1 ) ;
+inputB.connectTo( hidden12 , 1 ) ;
 
-hidden1.connectTo( outputA , 1 ) ;
-hidden1.connectTo( outputB , 1 ) ;
-hidden2.connectTo( outputA , 1 ) ;
-hidden2.connectTo( outputB , 1 ) ;
+hidden11.connectTo( hidden21 , 1 ) ;
+hidden11.connectTo( hidden22 , 1 ) ;
+hidden12.connectTo( hidden21 , 1 ) ;
+hidden12.connectTo( hidden22 , 1 ) ;
+
+hidden21.connectTo( outputA , 1 ) ;
+hidden21.connectTo( outputB , 1 ) ;
+hidden22.connectTo( outputA , 1 ) ;
+hidden22.connectTo( outputB , 1 ) ;
 
 network.feedForwardOrder() ;
 
-console.log( network.neuronOrder.indexOf( hidden1 ) ) ;
-console.log( network.neuronOrder.indexOf( hidden2 ) ) ;
+console.log( network.neuronOrder.indexOf( hidden11 ) ) ;
+console.log( network.neuronOrder.indexOf( hidden12 ) ) ;
+console.log( network.neuronOrder.indexOf( hidden21 ) ) ;
+console.log( network.neuronOrder.indexOf( hidden22 ) ) ;
 console.log( network.neuronOrder.indexOf( outputA ) ) ;
 console.log( network.neuronOrder.indexOf( outputB ) ) ;
 
--- test/neuronsKit-test.js
+++ test/neuronsKit-test.js
@@ -38,6 +38,85 @@ var expect = require( 'expect.js' ) ;
 
 
 
+describe( "Network" , function() {
+	
+	it( "Feed forward order" , function() {
+		
+		var network = nk.createNetwork() ;
+		
+		var inputA = nk.createSignalEmitter() ,
+			inputB = nk.createSignalEmitter() ;
+		
+		var outputA = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
+			outputB = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ;
+		
+		var	hidden11 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
+			hidden12 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ;
+		
+		var	hidden21 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
+			hidden22 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ;
+		
+		var	hidden31 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ,
+			hidden32 = nk.createNeuron( { transfer: 'step' , threshold: 0 } ) ;
+		
+		network.addInput( 'a' , inputA ) ;
+		network.addInput( 'b' , inputB ) ;
+		network.addOutput( 'outputA' , outputA ) ;
+		network.addOutput( 'outputB' , outputB ) ;
+		network.addHiddenNeuron( hidden21 ) ;
+		network.addHiddenNeuron( hidden22 ) ;
+		network.addHiddenNeuron( hidden11 ) ;
+		network.addHiddenNeuron( hidden12 ) ;
+		network.addHiddenNeuron( hidden31 ) ;
+		network.addHiddenNeuron( hidden32 ) ;
+		
+		inputA.connectTo( hidden11 , 1 ) ;
+		inputA.connectTo( hidden12 , 1 ) ;
+		inputB.connectTo( hidden11 , 1 ) ;
+		inputB.connectTo( hidden12 , 1 ) ;
+		
+		hidden11.connectTo( hidden21 , 1 ) ;
+		hidden11.connectTo( hidden22 , 1 ) ;
+		hidden12.connectTo( hidden21 , 1 ) ;
+		hidden12.connectTo( hidden22 , 1 ) ;
+		
+		hidden21.connectTo( hidden31 , 1 ) ;
+		hidden21.connectTo( hidden32 , 1 ) ;
+		hidden22.connectTo( hidden31 , 1 ) ;
+		hidden22.connectTo( hidden32 , 1 ) ;
+		
+		hidden31.connectTo( outputA , 1 ) ;
+		hidden31.connectTo( outputB , 1 ) ;
+		hidden32.connectTo( outputA , 1 ) ;
+		hidden32.connectTo( outputB , 1 ) ;
+		
+		network.feedForwardOrder() ;
+		
+		expect( network.neuronOrder.indexOf( hidden11 ) ).to.be.within( 0 , 1 ) ;
+		expect( network.neuronOrder.indexOf( hidden12 ) ).to.be.within( 0 , 1 ) ;
+		expect( network.neuronOrder.indexOf( hidden21 ) ).to.be.within( 2 , 3 ) ;
+		expect( network.neuronOrder.indexOf( hidden22 ) ).to.be.within( 2 , 3 ) ;
+		expect( network.neuronOrder.indexOf( hidden31 ) ).to.be.within( 4 , 5 ) ;
+		expect( network.neuronOrder.indexOf( hidden32 ) ).to.be.within( 4 , 5 ) ;
+		expect( network.neuronOrder.indexOf( outputA ) ).to.be.within( 6 , 7 ) ;
+		expect( network.neuronOrder.indexOf( outputB ) ).to.be.within( 6 , 7 ) ;
+		
+		// Create a loop
+		hidden32.connectTo( hidden21 , 1 ) ;
+		
+		// So .feedForwardOrder() should throw an error...
+		try {
+			network.feedForwardOrder() ;
+			expect().fail( 'It should throw!' ) ;
+		}
+		catch( error ) {
+		}
+	} ) ;
+} ) ;
+
+
+
+/*
 describe( "..." , function() {
 	
 	it( "..." , function() {
@@ -57,4 +136,5 @@ describe( "..." , function() {
 		console.log( neuron.signal ) ;
 	} ) ;
 } ) ;
+*/
 
