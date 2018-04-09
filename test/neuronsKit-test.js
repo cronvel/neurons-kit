239aed3a04973c00f18a79d4033f6d34378061b0
Cédric Ronvel <cedric.ronvel@gmail.com>
2018-04-09 14:37:13 +0200

    wip

--- test/neuronsKit-test.js
+++ test/neuronsKit-test.js
@@ -1,20 +1,20 @@
 /*
-	The Cedric's Swiss Knife (CSK) - CSK neurons toolbox
-
-	Copyright (c) 2015 Cédric Ronvel 
+	Neurons Kit
+	
+	Copyright (c) 2015 - 2018 Cédric Ronvel
 	
 	The MIT License (MIT)
-
+	
 	Permission is hereby granted, free of charge, to any person obtaining a copy
 	of this software and associated documentation files (the "Software"), to deal
 	in the Software without restriction, including without limitation the rights
 	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 	copies of the Software, and to permit persons to whom the Software is
 	furnished to do so, subject to the following conditions:
-
+	
 	The above copyright notice and this permission notice shall be included in all
 	copies or substantial portions of the Software.
-
+	
 	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,25 +24,11 @@
 	SOFTWARE.
 */
 
-/* jshint unused:false */
-/* global describe, it, before, after */
+"use strict" ;
 
 
 
 var expect = require( 'expect.js' ) ;
-
-var args = require( 'minimist' )( process.argv.slice( 2 ) ) ;
-var spp , nk ;
-
-if ( args.spp )
-{
-	spp = require( 'smart-preprocessor' ) ;
-	nk = spp.require( __dirname + '/../lib/neuronsKit.js' , args.spp ) ;
-}
-else
-{
-	nk = require( '../lib/neuronsKit.js' ) ;
-}
 
 
 
